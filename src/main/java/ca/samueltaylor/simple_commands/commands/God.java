9cc379ca75dcfb2e1008258ec0337aafbe7f9e40
Sam Taylor <sam@taylornetwork.ca>
2021-07-02 12:17:16 -0400

    Added optional player args to fly, heal. god commands

--- src/main/java/ca/samueltaylor/simple_commands/commands/God.java
+++ src/main/java/ca/samueltaylor/simple_commands/commands/God.java
@@ -1,27 +1,42 @@
 package ca.samueltaylor.simple_commands.commands;
 
-import ca.samueltaylor.simple_commands.abstract_commands.BaseCommand;
+import ca.samueltaylor.simple_commands.abstract_commands.OptionalPlayerArgument;
 import ca.samueltaylor.simple_commands.helpers.Chat;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.command.ServerCommandSource;
 
-public class God extends BaseCommand {
+public class God extends OptionalPlayerArgument {
     public God() {
         command = "god";
+        argumentName = "target";
     }
 
     @Override
     public int execute(CommandContext<ServerCommandSource> commandContext) throws CommandSyntaxException {
-        PlayerEntity player = commandContext.getSource().getPlayer();
-        Chat chat = new Chat(player);
-        boolean toggle = !player.getAbilities().invulnerable;
+        return this.run(commandContext, commandContext.getSource().getPlayer());
+    }
 
-        player.getAbilities().invulnerable = toggle;
-        player.sendAbilitiesUpdate();
-        chat.send("God mode " + (toggle ? "enabled!" : "disabled!"));
+    @Override
+    public int executeWithArgument(CommandContext<ServerCommandSource> commandContext) throws CommandSyntaxException {
+        return this.run(commandContext, EntityArgumentType.getPlayer(commandContext, argumentName));
+    }
+
+    public int run(CommandContext<ServerCommandSource> commandContext, PlayerEntity target) throws CommandSyntaxException {
+        PlayerEntity player = commandContext.getSource().getPlayer();
+        boolean toggle = !target.getAbilities().invulnerable;
+
+        target.getAbilities().invulnerable = toggle;
+        target.sendAbilitiesUpdate();
+
+        Chat.send(target, "You are " + (toggle ? "now" : "no longer") + " invulnerable!");
+
+        if(!target.equals(player)) {
+            Chat.send(player, (toggle ? "Enabled" : "Disabled") + " god mode for " + target.getName().getString());
+        }
 
         this.logCommand(commandContext);
         return Command.SINGLE_SUCCESS;
